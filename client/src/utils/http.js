1bc23eb81f09dbbed041ac8016915767e774e5a2
Janwry Dela Cruz <jbenedicto20.jbb@gmail.com>
2023-07-15 20:45:33 +0800

    update baseURL

--- client/src/utils/http.js
+++ client/src/utils/http.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
     // baseURL: "http://localhost:3001/api/"
-    baseURL: "https://jb-portfolio-server.onrender.com/api/"
+    // baseURL: "https://jb-portfolio-server.onrender.com/api/"
+    baseURL: process.env.baseURL;
 })
 
 export default instance;
