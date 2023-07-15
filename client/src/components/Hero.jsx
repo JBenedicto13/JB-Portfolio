c1f9ad11fc4233206e3ed4acf2eadd4c54cca304
Janwry Dela Cruz <jbenedicto20.jbb@gmail.com>
2023-07-15 11:07:13 +0800

    Add button and change baseURL

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -18,6 +18,10 @@ function Hero() {
               Web Developer
               and Graphic Designer
           </p>
+          <div id='btnResume'>
+            <span id='resume-svg'><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path fill="#f3f3f3" d="M6 20q-.825 0-1.413-.588T4 18v-3h2v3h12v-3h2v3q0 .825-.588 1.413T18 20H6Zm6-4l-5-5l1.4-1.45l2.6 2.6V4h2v8.15l2.6-2.6L17 11l-5 5Z"/></svg></span>
+            <span id='resume-text'>Resume</span>
+          </div>
         </div>
       </div>
     </div>
--- client/src/utils/http.js
+++ client/src/utils/http.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://localhost:3001/api/"
-    // baseURL: "https://jb-portfolio-server.onrender.com:3001/api/"
+    // baseURL: "http://localhost:3001/api/"
+    baseURL: "https://jb-portfolio-server.onrender.com:1000/api/"
 })
 
 export default instance;
