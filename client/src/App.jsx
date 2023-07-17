9503aa58a2e4c3b01593d6655ca8b6fa303804fa
Janwry Dela Cruz <jbenedicto20.jbb@gmail.com>
2023-07-17 21:40:04 +0800

    Add scroll to top

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Routing from "./components/routing/Routing";
 import Navbar from './components/Navbar';
 import {BrowserRouter} from 'react-router-dom';
+import ScrollToTopButton from './components/ScrollToTopButton';
 
 function App() {
   
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
         <div className='main'>
           <Routing />
+          <ScrollToTopButton />
         </div>
       </div>
     </BrowserRouter>
---
+++ client/src/components/ScrollToTopButton.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import '../styles/scroll-to-top-button.css';
+
+const ScrollToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+    setIsVisible(scrollTop > 100);
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <button
+      className={`scroll-to-top ${isVisible ? 'show' : 'hide'}`}
+      onClick={scrollToTop}
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#111089" d="M3 19h18a1.002 1.002 0 0 0 .823-1.569l-9-13c-.373-.539-1.271-.539-1.645 0l-9 13A.999.999 0 0 0 3 19z"/></svg>
+    </button>
+  );
+};
+
+export default ScrollToTopButton;
