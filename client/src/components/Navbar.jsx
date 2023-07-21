d7fc3e5062369b2392e0829c9a907fe3e09376df
Janwry Dela Cruz <jbenedicto20.jbb@gmail.com>
2023-07-21 22:55:08 +0800

    update navbar menu

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,17 +1,37 @@
 import '../styles/navbar.css';
 import myLogo from "../assets/my-logo-black.png";
+import myLogoWhite from "../assets/JB-logo-white.png";
+import { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function Navbar() {
     const navigate = useNavigate();
+    const navbarRef = useRef(null);
+    const logoRef = useRef(null);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
+
     return (
         <nav className='navbar'>
-            <span onClick={() => navigate('/')}><img id='nav-logo' src={myLogo} alt="my-logo" /></span>
-            <ul className='navbar-nav'>
-                <li><a href="/#hero-section">Home</a></li>
-                <li><a href="/#works-section">Works</a></li>
-                <li><a href="/#about-section">About</a></li>
+            <span onClick={() => navigate('/')}><img ref={logoRef} id='nav-logo' src={isMenuOpen ? myLogoWhite : myLogo} alt="my-logo" /></span>
+            <ul ref={navbarRef} className={isMenuOpen ? 'navbar-nav-mobile' : 'navbar-nav'}>
+                <li onClick={isMenuOpen ? toggleMenu : ''}><a href="/#hero-section">Home</a></li>
+                <li onClick={isMenuOpen ? toggleMenu : ''}><a href="/#works-section">Works</a></li>
+                <li onClick={isMenuOpen ? toggleMenu : ''}><a href="/#about-section">About</a></li>
             </ul>
+            <span 
+                onClick={toggleMenu} 
+                className='menu-icon'
+            >
+                { 
+                    isMenuOpen 
+                    ? <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" fill-rule="evenodd"><path d="M24 0v24H0V0h24ZM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018Zm.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022Zm-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01l-.184-.092Z"/><path fill="#f3f3f3" d="m12 14.121l5.303 5.304a1.5 1.5 0 0 0 2.122-2.122L14.12 12l5.304-5.303a1.5 1.5 0 1 0-2.122-2.121L12 9.879L6.697 4.576a1.5 1.5 0 1 0-2.122 2.12L9.88 12l-5.304 5.303a1.5 1.5 0 1 0 2.122 2.122L12 14.12Z"/></g></svg>
+                    : <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none"><path d="M24 0v24H0V0h24ZM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018Zm.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022Zm-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01l-.184-.092Z"/><path fill="currentColor" d="M20 17.5a1.5 1.5 0 0 1 .144 2.993L20 20.5H4a1.5 1.5 0 0 1-.144-2.993L4 17.5h16Zm0-7a1.5 1.5 0 0 1 0 3H4a1.5 1.5 0 0 1 0-3h16Zm0-7a1.5 1.5 0 0 1 0 3H4a1.5 1.5 0 1 1 0-3h16Z"/></g></svg> 
+                }
+            </span>
         </nav>
     );
   }
