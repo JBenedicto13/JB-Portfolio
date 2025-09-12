2f390653ca9ad0dde1ca9ee58baa151b6e4fb0c6
JB Benedicto <jbenedicto20.jbb@gmail.com>
2025-09-13 00:06:25 +0800

    Enhance Vite configuration for build optimization and include react-gtm-module

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [],
+      output: {
+        globals: {}
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['react-gtm-module']
+  }
 })
